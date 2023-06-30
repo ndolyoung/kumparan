2c56b45e86a33dfe3e06d19427efac805e59d5f8
Ndol Young <66969523+ndolyoung@users.noreply.github.com>
2023-07-01 03:38:14 +0700

    Create center-category2.js

---
+++ center-category2.js
@@ -0,0 +1,2 @@
+function replaceText(){if(!document.getElementById){return}
+bodyText=document.getElementById("center-category2");theText=bodyText.innerHTML;theText=theText.replace(/default/gi,"hqdefault");bodyText.innerHTML=theText}replaceText()
